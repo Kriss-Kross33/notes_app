3e6c9b570e6fd700485ad78b2abffd90ada1e23c
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-07-27 21:01:54 +0000

    add body preview and date to card

--- lib/src/common/widgets/note_item.dart
+++ lib/src/common/widgets/note_item.dart
@@ -13,30 +13,74 @@ class _NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final date = note.updated ?? note.created;
+    final dateTime = DateFormat.yMMMd().add_jm().format(date!);
     return GestureDetector(
       key: Key('note_item_$index'),
       onTap: () => context.pushNamed(RouteConsts.noteDetailRoute, extra: note),
-      child: Container(
-        padding: const EdgeInsets.only(
-          left: 45,
-          right: 45,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          minHeight: 110,
+          minWidth: 500,
+          maxHeight: 250,
         ),
-        width: double.infinity,
-        height: 110,
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Center(
-          child: Text(
-            note.title,
-            maxLines: 3,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              height: 1,
-              fontSize: 23,
-              fontWeight: FontWeight.w400,
-              color: Colors.black,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 10,
+            ),
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      note.title,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        height: 1,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      note.body,
+                      maxLines: 8,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        height: 1,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: AppTheme.darkGrey,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: Text(
+                    dateTime,
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: AppTheme.darkGrey,
+                        ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
